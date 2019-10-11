baac7fd2aee1751086b1102fdddcd492d451881f
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-11 10:37:46 -0400

    styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, {useState, useEffect} from "react";
+import styled from 'styled-components';
 import axios from 'axios';
 import "./App.css";
+
+const Wrap = styled.div`
+  background: #F4D6B4;
+  color: black;
+  width: 425px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0px 1px 6px -2px grey;
+`;
+const Pic = styled.img`
+  width: 400px;
+  height: 400px;
+  object-fit: none;
+`;
+const Wordy = styled.p`
+  display: flex;
+  width: 425px;
+  overflow: none
+`;
 
 function App() {
   const [pic, setPic] = useState({});
@@ -18,17 +42,17 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <Wrap>
         <h2>Some of my Favorites</h2>
           <select name='date'>
             <option value=''>Favorite?</option>
             <option value='2018-05-21'>May 21 2018</option>
           </select>
           <h3>{pic.title}</h3>
-          <p><img src={pic.url} alt='amazing cosmos'></img></p>
-          <p>{pic.explanation}</p>
+          <Pic src={pic.url} alt='amazing cosmos'></Pic>
+          <Wordy>{pic.explanation}</Wordy>
           <small>{pic.date}</small>
-      </div>
+      </Wrap>
     </div>
   );
 }
