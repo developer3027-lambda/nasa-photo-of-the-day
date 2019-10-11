b16ade176d9c24fbe770c3911861ae14f02f7694
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-11 09:54:28 -0400

    page complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [pic, setPic] = useState({});
+
+  useEffect(() => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=7WdPhWpxKxiwRwJjWkJMleXMT2l2gh5bJpQ629we')
+    .then(response => {
+      setPic(response.data);
+    })
+    .catch(error => {
+      console.log('axios call: ', error);
+    })
+  },[])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div>
+        <h2>Some of my Favorites</h2>
+          <select name='date'>
+            <option value=''>Favorite?</option>
+            <option value='2018-05-21'>May 21 2018</option>
+          </select>
+          <h3>{pic.title}</h3>
+          <p><img src={pic.url} alt='amazing cosmos'></img></p>
+          <p>{pic.explanation}</p>
+          <small>{pic.date}</small>
+      </div>
     </div>
   );
 }
---
+++ src/PhotoOday.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PhotoOday(props) {
+
+    return (
+        <div>
+        <h2>{props.title}</h2>
+        <p>Image</p>
+        <p>description</p>
+        <p>Date</p>
+        </div>
+    );
+}
+
+export default PhotoOday
